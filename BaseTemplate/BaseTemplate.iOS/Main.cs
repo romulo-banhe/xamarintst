6c06d8b47f9768d30aee0c1602f9f2c6c33e2169
Romulo Banhe <romulobanhe@microsoft.com>
2022-03-03 14:53:24 -0300

    main changes

--- BaseTemplate/BaseTemplate.iOS/Main.cs
+++ BaseTemplate/BaseTemplate.iOS/Main.cs
@@ -9,7 +9,6 @@ namespace BaseTemplate.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
         }
     }
 }
